aabe60cda9cf2c72ff6a3fc6782a981cfb2472bd
Saif Sahriar <119245305+saifsahriar@users.noreply.github.com>
2024-02-29 13:33:38 +0530

    Create remove.sh
    Removes temporary files older than 7 days from the temp directory

---
+++ remove.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+TARGET_DIR="/tmp"
+DAYS=7
+OLD_FILES=$(find  "$TARGET_DIR" -type f -mtime +$DAYS)
+
+if [ -z "${OLD_FILES[0]}" ]; then
+        echo "No files found"
+        exit 0
+fi
+
+echo "Files to be deleted:"
+echo "$OLD_FILES"
+
+read -p "Are you sure you want to delete these files?[y/n]" ANSWER
+
+if [[ $ANSWER != "y" ]]; then
+        echo "Canceled"
+        exit 0
+fi
+
+echo "Deleting files..."
+rm "$OLD_FILES"
+
+sleep 1
+
+echo "Files deleted"
